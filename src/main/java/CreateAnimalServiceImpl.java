0415850ad0ef4a371208cc3a58b8028753ca4eb6
mvpost <1029345a>
2024-04-02 16:40:32 +0300

    Отдельные правки в сервисе

--- src/main/java/CreateAnimalServiceImpl.java
+++ src/main/java/CreateAnimalServiceImpl.java
@@ -1,26 +1,17 @@
 public class CreateAnimalServiceImpl extends CreateAnimalService {
-    public static void createAnimals(int N) {
+    public void createAnimals(int N) {
         System.out.println("Create Animal with cycle for:");
         for (int i = 0; i < N; i++) {
             Animal animal;
-            byte remainder = (byte) (N % 4);
-            switch (remainder) {
-                case 3:
-                    animal = new Wolf();
-                    break;
-                case 2:
-                    animal = new Shark();
-                    break;
-                case 1:
-                    animal = new Cat();
-                    break;
-                case 0:
-                    animal = new Dog();
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + remainder);
-            }
-            System.out.println("Created a new animal " + i + ": " + animal.getName());
+            byte remainder = (byte) (i % 4);
+            animal = switch (remainder) {
+                case 3 -> new Wolf();
+                case 2 -> new Shark();
+                case 1 -> new Cat();
+                case 0 -> new Dog();
+                default -> throw new IllegalStateException("Unexpected value: " + remainder);
+            };
+            System.out.printf("Created a new animal %d : %s \n", i, animal.getName());
         }
     }
 
@@ -32,24 +23,15 @@ public class CreateAnimalServiceImpl extends CreateAnimalService {
         do {
             Animal animal;
             byte remainder = (byte) (count % 4);
-            switch (remainder) {
-                case 3:
-                    animal = new Wolf();
-                    break;
-                case 2:
-                    animal = new Shark();
-                    break;
-                case 1:
-                    animal = new Cat();
-                    break;
-                case 0:
-                    animal = new Dog();
-                    break;
-                default:
-                    throw new IllegalStateException("Unexpected value: " + remainder);
-            }
+            animal = switch (remainder) {
+                case 3 -> new Wolf();
+                case 2 -> new Shark();
+                case 1 -> new Cat();
+                case 0 -> new Dog();
+                default -> throw new IllegalStateException("Unexpected value: " + remainder);
+            };
 
-            System.out.println("Created a new animal " + count + ": " + animal.getName());
+            System.out.printf("Created a new animal %d : %s \n", count, animal.getName());
             count++;
         }
         while (count < 10);
