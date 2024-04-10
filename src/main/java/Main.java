168325ddb29d4a0ed96bcb448526119af7a512cc
mvpost <1029345a>
2024-04-10 17:24:37 +0300

    удалил неиспользуемые импорты

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,8 @@
-import java.time.LocalDate;
-import java.util.Arrays;
-
 public class Main {
     public static void main(String[] args){
         CreateAnimalService createAnimalService = new CreateAnimalService();
         CreateAnimalServiceImpl createAnimalServiceImpl = new CreateAnimalServiceImpl();
-        CheckLeapYearAnimalImpl checkLeapYearAnimalImpl = new CheckLeapYearAnimalImpl();
+
 
         createAnimalService.createAnimals();
         createAnimalServiceImpl.createAnimals(5);
